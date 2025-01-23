03a8e8fef1d871b1e68265328fe2c74e1da9117a
Md Shadman Mahfuz Raafi <156221826+Raafi8900@users.noreply.github.com>
2025-01-23 21:11:57 +0600

    update
    update

---
+++ Routing/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+//Home
+app.get("/", function (req, res) {
+    res.send("Hello Express js"); // Send the response directly
+});
+
+//about
+app.get("/about", function (req, res) {
+    res.send("About Page"); // Send the response directly
+});
+
+//Contact 
+app.get("/contact", function (req, res) {
+    res.send("Contact Page"); // Send the response directly
+});
+
+//Terms
+app.get("/terms", function (req, res) {
+    res.send("Term page"); // Send the response directly
+});
+
+app.listen(8000, function () {
+    console.log("Server running successfully on port 8000");
+});
